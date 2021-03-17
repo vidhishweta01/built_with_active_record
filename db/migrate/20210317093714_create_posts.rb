7aa79ba86dafbf3861d2e343c6d3e04f724aae3f
vidhishweta01 <vidhishweta01@gmail.com>
2021-03-17 15:11:11 +0530

    generated models author, post, comment

---
+++ db/migrate/20210317093714_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :body
+      t.integer :userid
+
+      t.timestamps
+    end
+  end
+end
